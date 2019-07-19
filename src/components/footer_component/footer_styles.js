45b61ee5d445a4d81fea177a4e4c2ae2a06b9ac6
enriccamacho <enriccamachora@gmail.com>
2019-07-19 19:55:53 +0200

    small fixes

--- src/components/footer_component/footer_styles.js
+++ src/components/footer_component/footer_styles.js
@@ -4,15 +4,14 @@ import {unitHeight, unitWidth} from "../../constants/dimensions"
 export default StyleSheet.create({
 
     footerContainer:{
-        width: unitWidth*10,
+        width: unitWidth * 10,
         height: 50,
         backgroundColor: "#F2F2F2",
-        borderRadius: 20,
         display: "flex",
         flexDirection: "row"
     },
     footerItem:{
-        width: unitWidth*2.5,
+        width: unitWidth * 2.5,
         height: 50,
         display: "flex",
         justifyContent: "center",
--- src/components/header_component/header_styles.js
+++ src/components/header_component/header_styles.js
@@ -4,14 +4,14 @@ import {unitHeight, unitWidth} from "../../constants/dimensions"
 export default StyleSheet.create({
 
     headerContainer:{
-        width: unitWidth*10,
+        width: unitWidth * 10,
         height: 80,
         backgroundColor: "#F2F2F2",
         display: "flex",
         flexDirection: "row"
     },
     headerItem:{
-        width: unitWidth*5,
+        width: unitWidth * 5,
         height: 100,
         display: "flex",
         justifyContent: "center",

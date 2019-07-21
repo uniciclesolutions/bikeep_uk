2a0915e6b6befd9d7ad65f47a31eb71ced2ad559
Oscar Crespo <oscar@MacBook-Pro-de-Oscar.local>
2019-07-21 19:04:20 +0200

    sign in hooks + state values implemented

--- src/screens/auth/signIn/signIn_screen.js
+++ src/screens/auth/signIn/signIn_screen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./signIn_screen_styles";
 import {
   View,
@@ -23,61 +23,61 @@ import {
 
 import routes from "../../../router/routes";
 
-export default class SignInScreen extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      labelValue: "",
-      checked: false
-    };
-  }
-  onValueChange2(value) {
-    this.setState({
-      labelValue: value
-    });
-  }
-  render() {
-    return (
-      <View style={styles.background}>
-        <View style={styles.signInContainer}>
-          <Image
-            style={styles.circularPic}
-            resizeMode="cover"
-            source={require("../../../../assets/bikeep.png")}
+const SignInScreen = props => {
+  const [checked, setChecked] = useState(false);
+  const [inputValue, setInputValue] = useState(undefined);
+  const changeCheckValue = () => {
+    if (!checked) {
+      setChecked(true);
+    } else {
+      setChecked(false);
+    }
+  };
+  return (
+    <View style={styles.background}>
+      <View style={styles.signInContainer}>
+        <Image
+          style={styles.circularPic}
+          resizeMode="cover"
+          source={require("../../../../assets/bikeep.png")}
+        />
+        <Text style={styles.title}>START BIKEEP</Text>
+        <Text style={styles.subtitle}>
+          We will send you an SMS message to validate your phone number
+        </Text>
+        <Item>
+          <Icon style={{ color: "white" }} active name="call" />
+          {!inputValue && <Label>Phone Number</Label>}
+          <Input
+            style={{ color: "white" }}
+            onChangeText={() => setInputValue(this)}
           />
-          <Text style={styles.title}>START BIKEEP</Text>
-          <Text style={styles.subtitle}>
-            We will send you an SMS message to validate your phone number
+        </Item>
+        <View style={styles.agreeContainer}>
+          <CheckBox
+            checked={checked}
+            color="red"
+            onPress={() => changeCheckValue()}
+          />
+          <Text style={{ color: "white", marginLeft: 20 }}>
+            I confirm that I am 18 years old or older
           </Text>
-          <Item>
-            <Icon style={{ color: "white" }} active name="call" />
-            {this.state.labelValue == "" && <Label>Phone Number</Label>}
-            <Input
-              style={{ color: "white" }}
-              onValueChange={this.onValueChange2.bind(this)}
-            />
-            <Text>{this.state.labelValue}</Text>
-          </Item>
-          <View style={styles.agreeContainer}>
-            <CheckBox checked={this.state.checked} color="red" />
-            <Text style={{ color: "white", marginLeft: 20 }}>
-              I confirm that I am 18 years old or older
-            </Text>
-          </View>
-          <View style={styles.confirmButton}>
-            <Button
-              full
-              rounded
-              style={{
-                backgroundColor: "rgba(253,187,45,1) "
-              }}
-              onPress={() => this.props.navigation.navigate(routes.home())}
-            >
-              <Text> Continue </Text>
-            </Button>
-          </View>
+        </View>
+        <View style={styles.confirmButton}>
+          <Button
+            full
+            rounded
+            style={{
+              backgroundColor: "rgba(253,187,45,1) "
+            }}
+            onPress={() => this.props.navigation.navigate(routes.home())}
+          >
+            <Text> Continue </Text>
+          </Button>
         </View>
       </View>
-    );
-  }
-}
+    </View>
+  );
+};
+
+export default SignInScreen;
